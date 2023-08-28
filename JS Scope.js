695903d22cc05ecb6b97dcb26b9f776fe1ab7ee2
imran-it1 <imranhossainsakil22@gmail.com>
2023-08-29 02:39:14 +0600

    JS duniya

--- JS Scope.js
+++ JS Scope.js
@@ -3,12 +3,14 @@
 var x = 5;
 
 function print() {
-  var x = 10;
+  //   var x = 10;
+  //   var y = 10;
   //   var x = 15;
   console.log(`Inside function `, x);
 }
 
 // Window is matter of Browser. Need browser to see the result
+// const/let diye declared korle abar access korte parba na. Because const/let variable are not added to the window object
 console.log(window.x);
 print();
 // Prove that X variable are in window object or GEC
