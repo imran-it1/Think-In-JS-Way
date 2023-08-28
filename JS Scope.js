49bbe18fa94e097d0b0469d284f01cb06bd7d7c8
imran-it1 <imranhossainsakil22@gmail.com>
2023-08-29 02:14:29 +0600

    first commit

---
+++ JS Scope.js
@@ -0,0 +1,10 @@
+var x = 5;
+function print() {
+  console.log(`Inside function `, x);
+}
+
+// Window is matter of Browser. Need browser to see the result
+console.log(x);
+// Prove that X variable are in window object or GEC
+console.log(window.x);
+// print();
