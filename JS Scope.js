bce6b80dd30fd22f77db59ddada32080491b841d
imran-it1 <imranhossainsakil22@gmail.com>
2023-08-29 02:29:04 +0600

    JS duniya

--- JS Scope.js
+++ JS Scope.js
@@ -1,10 +1,15 @@
+// "use strict";
+
 var x = 5;
+
 function print() {
+  var x = 10;
+  //   var x = 15;
   console.log(`Inside function `, x);
 }
 
 // Window is matter of Browser. Need browser to see the result
-console.log(x);
-// Prove that X variable are in window object or GEC
 console.log(window.x);
-// print();
+print();
+// Prove that X variable are in window object or GEC
+// console.log(window.x);
