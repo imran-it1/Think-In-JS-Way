6e2c52b6cb66272a129945ba42305545aa64e34a
imran-it1 <imranhossainsakil22@gmail.com>
2023-08-31 15:03:25 +0600

    Some practice

--- async await.js
+++ async await.js
@@ -1,13 +1,13 @@
-function makeRequest(location) {
-  return new Promise((resolve, reject) => {
-    console.log(`Making request to ${location}`);
-    if (location === "google") {
-      resolve("Hello Google");
-    } else {
-      reject("We only accept google");
-    }
-  });
-}
+// function makeRequest(location) {
+//   return new Promise((resolve, reject) => {
+//     console.log(`Making request to ${location}`);
+//     if (location === "google") {
+//       resolve("Hello Google");
+//     } else {
+//       reject("We only accept google");
+//     }
+//   });
+// }
 
 // makeRequest("google")
 //   .then((messaeg) => {
@@ -17,38 +17,73 @@ function makeRequest(location) {
 //     console.log(message);
 //   });
 
-function processRequest(response) {
-  return new Promise((resolve, reject) => {
-    console.log("Processing response");
-    resolve(`Extra information ${response}`);
-  });
-}
+// function processRequest(response) {
+//   return new Promise((resolve, reject) => {
+//     console.log("Processing response");
+//     resolve(`Extra information ${response}`);
+//   });
+// }
 
-// processRequest("Baler Information").then((messages) => {
-//   console.log(messages);
-// });
+// // processRequest("Baler Information").then((messages) => {
+// //   console.log(messages);
+// // });
 
-makeRequest("google")
-  .then((response) => {
-    //   console.log(response);
-    console.log("Response Received");
-    return processRequest(response);
-  })
-  .then((processedResponse) => {
-    console.log(processedResponse);
-  });
+// makeRequest("google")
+//   .then((response) => {
+//     //   console.log(response);
+//     console.log("Response Received");
+//     return processRequest(response);
+//   })
+//   .then((processedResponse) => {
+//     console.log(processedResponse);
+//   });
 
-async function calldata() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
+// async function calldata() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+//   }
+// }
+
+// calldata();
+
+// console.log("I am from last");
+
+// async await it the better version of promise
+
+new Promise((resolve, reject) => {
+  fetch("https://randomuser.me/api/")
+    .then((response) => {
+      return response;
+    })
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      return resolve(data);
+    })
+    .catch(() => {
+      return reject();
+    });
+})
+  .then((data) => {
     console.log(data);
-  } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
-  }
+  })
+  .catch(() => console.log("Something Went Wrong"));
+
+async function getdata() {
+  const response = await fetch("https://randomuser.me/api/");
+  const data = await response.json();
+  console.log(data);
+  console.log("imran");
 }
 
-calldata();
+getdata();
 
-console.log("I am from last");
+// async await Promise er boro khalato vai. Promise a je bar bar .then likha lage seta theke mukti mele aync await abort
+// .then diye result na dhore, asaync await a promise we response amra variable a dhorte pari.
+// awiat use kore amra data na paoa obdi opekkha korte boli. Na hole data na pele, show korbo ki.
--- try catch throw finally.js
+++ try catch throw finally.js
@@ -1,35 +1,43 @@
-try {
-  let age = aler("Enter our age please");
-} catch (error) {
-  console.log(error.name);
-  console.log(error.message);
-}
+// try {
+//   let age = aler("Enter our age please");
+// } catch (error) {
+//   console.log(error.name);
+//   console.log(error.message);
+// }
 
-try {
-  setTimeout(() => {
-    try {
-      console.log(imran);
-    } catch (error) {
-      console.log("Something Wrong");
-    }
-  }, 1000);
-} catch (error) {
-  console.log(error.name);
-  console.log(error.message);
-}
+// try {
+//   setTimeout(() => {
+//     try {
+//       console.log(imran);
+//     } catch (error) {
+//       console.log("Something Wrong");
+//     }
+//   }, 1000);
+// } catch (error) {
+//   console.log(error.name);
+//   console.log(error.message);
+// }
 
-console.log("imran");
+// console.log("imran");
 
-try {
-  let age = 25;
-  if (age < 18) {
-    throw new Error("baccha kaccha not allow");
-  } else {
-    console.log("You are allowed");
-  }
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   let age = 25;
+//   if (age < 18) {
+//     throw new Error("baccha kaccha not allow");
+//   } else {
+//     console.log("You are allowed");
+//   }
+// } catch (error) {
+//   console.log(error);
+// }
 
-try {
-} catch (error) {}
+// try {
+// } catch (error) {}
+
+// console.log("imran");
+// try {
+//   console.log(imran);
+// } catch (error) {
+//   console.log(error);
+// }
+// console.log("hossain");
